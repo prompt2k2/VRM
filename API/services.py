3eecd9a13ef601ad619f6ee45c9a6de5d527c856
prompt2k2 <prompt2k2@gmail.com>
2021-07-08 16:48:31 +0100

    env added

--- API/services.py
+++ API/services.py
@@ -2,9 +2,9 @@ import os
 import requests
 
 def get_droplets():
-    access_token = 'dfdfsdsfdsgskjksfhjshf'
+    
     url = 'https://vrmapi.victronenergy.com/v2/droplets'
-    r = requests.get(url, headers={'Authorization':'Bearer %s' % access_token})
+    r = requests.get(url, headers={'Authorization':'Bearer %s' % os.getenv('DO_ACCESS_TOKEN')})
     droplets = r.json()
     droplet_list = []
     for i in range(len(droplets['droplets'])):
